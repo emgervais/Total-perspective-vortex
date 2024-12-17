d54f867518a13a63630efd4ce88841cea1f30845
emile <guaitanverish2@outlook.com>
2024-12-17 21:00:47 +0100

    done

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from sklearn.model_selection import cross_val_score
 import joblib
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 
 RUNS = [[3, 7, 11], [4, 8, 12], [5, 9, 13], [6, 10, 14]]
 GOOD_CHANNELS = ["FC3", "FCz", "FC4", "C3", "C1", "Cz", "C2", "C4"]
@@ -55,7 +56,7 @@ def get_data(epochs):
     psd_data = epochs.compute_psd(
         fmin=7, fmax=30, method="multitaper"
     )
-    psds, freqs = psd_data.get_data(return_freqs=True)
+    psds, _ = psd_data.get_data(return_freqs=True)
     feature = psds.mean(axis=2)
     label = epochs.events[:, -1]
 
@@ -67,20 +68,21 @@ def all():
         features = []
         labels = []
         for subject in subject_list:
-            raws = []
-            for r in run:
-                raw = fetch_raw(subject, r)
-                raws.append(raw)
-            raw = preproccess_raw(raws)
-            epochs = get_epochs(raw)
-            feature, label = get_data(epochs)
+            try:
+                raws = []
+                for r in run:
+                    raw = fetch_raw(subject, r)
+                    raws.append(raw)
+                raw = preproccess_raw(raws)
+                epochs = get_epochs(raw)
+                feature, label = get_data(epochs)
 
-            features.append(feature)
-            labels.append(label)
+                features.append(feature)
+                labels.append(label)
+            except FileNotFoundError:
+                pass
         features = np.concatenate(features, axis=0)
         labels = np.concatenate(labels, axis=0)
-
-        X_train, X_test, y_train, y_test = train_test_split( features, labels, test_size=0.2, random_state=42)
         pipeline = Pipeline(
             [
                 ("scaler", StandardScaler()),
@@ -89,15 +91,15 @@ def all():
             ]
         )
         cv = ShuffleSplit(n_splits=5, test_size=0.2, random_state=42)
-        scores = cross_val_score(pipeline, X_train, y_train, cv=cv)
-        print("Cross-validation scores:", np.mean(scores))
+        scores = cross_val_score(pipeline, features, labels, cv=cv)
+        print("Cross-validation scores:", round(np.mean(scores), 2))
 
 def train(subject, run):
     raw = fetch_raw(subject, run)
     raw = preproccess_raw([raw])
     epochs = get_epochs(raw)
     feature, label = get_data(epochs)
-    X_train, X_test, y_train, y_test = train_test_split( feature, label, test_size=0.2, random_state=42)
+    X_train, _, y_train, _ = train_test_split( feature, label, test_size=0.2, random_state=42)
     pipeline = Pipeline(
         [
             ("scaler", StandardScaler()),
@@ -105,28 +107,39 @@ def train(subject, run):
             ("clf", LinearDiscriminantAnalysis()),
         ]
     )
-    scores = cross_val_score(pipeline, X_train, y_train, cv=5)
-    print("cross_val_score:", scores.mean())
+    scores = cross_val_score(pipeline, feature, label, cv=5)
+    print("cross_val_score:", round(scores.mean(), 2))
     pipeline.fit(X_train, y_train)
-    joblib.dump(pipeline, f"model_subject_{subject}_run_{run}.pkl")
+    joblib.dump(pipeline, f"model_subject_{subject}_run_{run}")
 
 def predict(subject, run):
     raw = fetch_raw(subject, run)
     raw = preproccess_raw([raw])
     epochs = get_epochs(raw)
     feature, label = get_data(epochs)
+    _, X_test, _, y_test = train_test_split( feature, label, test_size=0.2, random_state=42)
     try:
-        pipeline = joblib.load(f"model_subject_{subject}_run_{run}.pkl")
-    except:
-        print("Model not found. Please train the model first")
+        pipeline = joblib.load(f"model_subject_{subject}_run_{run}")
+    except ValueError:
+        print(f"Model for subject: {subject} run: {run} not found. Please train the model first")
         sys.exit(1)
-    score = pipeline.score(feature, label)
+    score = pipeline.score(X_test, y_test)
     print("epoch nb: [prediction] [truth] equal?")
-    for i, x, y in zip(range(len(feature)), feature, label):
+    for i, x, y in zip(range(len(X_test)), X_test, y_test):
         pred = pipeline.predict([x])
         print(f"Epoch {i}:  [{y}], {pred[0]}, {True if y == pred[0] else False}")
         time.sleep(0.5)
-    print("Test score:", score)
+    print("Test score:", round(score, 2))
+
+def viz(subject, run):
+    try:
+        raw = fetch_raw(subject, run)
+        raw.plot(show=True)
+        raw = preproccess_raw([raw])
+        raw.plot(show=True)
+        plt.show()
+    except:
+        print(f"Vizualization failed for subject: {subject} run: {run}")
 
 def main():
     msg = "\
@@ -135,12 +148,14 @@ def main():
         2. train: train the model on specific subjects\n\
         3. predict: predict the task on specific subjects and run\n\
     --subject: the subject number\n\
-    --run: the run number\n"
+    --run: the run number\n\
+    --viz: visualize the EEG data\n"
 
     parser = argparse.ArgumentParser(description = msg)
     parser.add_argument("--mode", type=str, default="all", help="Run all the subjects and all the tasks")
-    parser.add_argument("--subject", type=int, help="The subject number")
-    parser.add_argument("--run", type=int, help="The run number")
+    parser.add_argument("--subject", type=int, choices=range(1, 110), help="The subject number")
+    parser.add_argument("--run", type=int, choices=range(3,15), help="The run number")
+    parser.add_argument("--viz", type=bool, default=True, help="Visualize the EEG data")
     args = parser.parse_args()
     if len(sys.argv) == 1 or args.mode == "all":
         all()
@@ -149,12 +164,22 @@ def main():
             print("Please provide the subject number and a run number")
             sys.exit(1)
         elif args.mode == "train":
-            train(args.subject, args.run)
+            try:
+                train(args.subject, args.run)
+            except FileNotFoundError:
+                print(f"Data for subject: {args.subject} run: {args.run} not found")
+                sys.exit(1)
         elif args.mode == "predict":
-            predict(args.subject, args.run)
+            try:
+                predict(args.subject, args.run)
+            except FileNotFoundError:
+                print(f"Data for subject: {args.subject} run: {args.run} not found")
+                sys.exit(1)
         else:
             print("Invalid mode")
             sys.exit(1)
+        if args.viz:
+            viz(args.subject, args.run)
 
 
 if __name__ == "__main__":
